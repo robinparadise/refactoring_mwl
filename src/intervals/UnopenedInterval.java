2cc90501a679d95bef4e00c9f7e89cd3caab48cc
robinparadise <robinchogiles@gmail.com>
2015-04-09 23:58:02 +0100

    UnopenedInterval constructor

--- src/intervals/UnopenedInterval.java
+++ src/intervals/UnopenedInterval.java
@@ -1,5 +1,9 @@
 package intervals;
 
-public class UnopenedInterval {
+public class UnopenedInterval extends Interval {
+	
+	public UnopenedInterval(double minimum, double maximum, Opening opening) {
+		super(minimum, maximum, opening);
+	}
 
 }
