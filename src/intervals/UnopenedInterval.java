951c0c915ca05f26f8956cd33201239c23ac62e2
robinparadise <robinchogiles@gmail.com>
2015-04-09 23:59:52 +0100

    UnopenedInterval includes

--- src/intervals/UnopenedInterval.java
+++ src/intervals/UnopenedInterval.java
@@ -5,5 +5,9 @@ public class UnopenedInterval extends Interval {
 	public UnopenedInterval(double minimum, double maximum, Opening opening) {
 		super(minimum, maximum, opening);
 	}
+	
+	public boolean includes(double value) {
+		return getMinimum() <= value && value <= getMaximum();
+	}
 
 }
