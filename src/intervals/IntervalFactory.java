65985a8757a41635735a6b9426c30791140c4597
robinparadise <robinchogiles@gmail.com>
2015-04-10 18:34:23 +0100

    Factory with subclasses

--- src/intervals/IntervalFactory.java
+++ src/intervals/IntervalFactory.java
@@ -4,6 +4,19 @@ package intervals;
 public class IntervalFactory {
 
 	public static Interval getInterval(double minimum, double maximum, Opening opening) {
-		return new Interval(minimum, maximum, opening);
+	    
+    	switch (opening) {
+            case BOTH_OPENED:
+                return new BothOpenedInterval(minimum, maximum, opening);
+            case LEFT_OPENED:
+                return new LeftOpenedInterval(minimum, maximum, opening);
+            case RIGHT_OPENED:
+                return new RightOpenedInterval(minimum, maximum, opening);
+            case UNOPENED:
+                return new UnopenedInterval(minimum, maximum, opening);
+            default:
+                throw new IllegalArgumentException("Incorrect type code");
+        }
+		
 	}
 }
