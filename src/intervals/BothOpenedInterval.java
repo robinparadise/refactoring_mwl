e2aadbe036c494ebecbc4ac49fab481ca883fdbf
robinparadise <robinchogiles@gmail.com>
2015-04-10 19:50:00 +0100

    Extract intersects with :: BothOpenedInterval

--- src/intervals/BothOpenedInterval.java
+++ src/intervals/BothOpenedInterval.java
@@ -1,34 +1,47 @@
 package intervals;
 
 public class BothOpenedInterval extends Interval {
-	
+
 	public BothOpenedInterval(double minimum, double maximum, Opening opening) {
 		super(minimum, maximum, opening);
 	}
-	
+
 	public boolean includes(double value) {
 		return getMinimum() < value && value < getMaximum();
 	}
-	
+
 	public boolean includes(Interval interval) {
 		boolean minimumIncluded = this.includes(interval.getMinimum());
 		boolean maximumIncluded = this.includes(interval.getMaximum());
 		switch (interval.getOpening()) {
 		case BOTH_OPENED:
 			return (minimumIncluded || getMinimum() == interval.getMinimum())
-					&& (maximumIncluded || getMaximum() == interval.getMaximum());
+					&& (maximumIncluded || getMaximum() == interval
+							.getMaximum());
 		case LEFT_OPENED:
 			return (minimumIncluded || getMinimum() == interval.getMinimum())
 					&& (maximumIncluded);
 		case RIGHT_OPENED:
 			return (minimumIncluded)
-					&& (maximumIncluded || getMaximum() == interval.getMaximum());
+					&& (maximumIncluded || getMaximum() == interval
+							.getMaximum());
 		case UNOPENED:
 			return (minimumIncluded) && (maximumIncluded);
 		default:
 			assert false;
 			return false;
 		}
+	}
+
+	public boolean intersectsWith(Interval interval) {
+		if (getMinimum() == interval.getMaximum()) {
+			return false;
+		}
+		if (getMaximum() == interval.getMinimum()) {
+			return false;
+		}
+		return this.includes(interval.getMinimum())
+				|| this.includes(interval.getMaximum());
 	}
 
 }
