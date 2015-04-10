f54edbc213f06f0791733ad505729e73d618e317
robinparadise <robinchogiles@gmail.com>
2015-04-10 23:12:46 +0100

    BothOpenedInterval :: Fix includes interval for subclasses

--- src/intervals/BothOpenedInterval.java
+++ src/intervals/BothOpenedInterval.java
@@ -9,12 +9,42 @@ public class BothOpenedInterval extends Interval {
 	public boolean includes(double value) {
 		return getMinimum() < value && value < getMaximum();
 	}
-
+	
 	public boolean includes(Interval interval) {
+		return interval.isIncluded(this);
+	}
+	
+	@Override
+	public boolean isIncluded(BothOpenedInterval interval) {
 		boolean minimumIncluded = this.includes(interval.getMinimum());
 		boolean maximumIncluded = this.includes(interval.getMaximum());
 		return (minimumIncluded || getMinimum() == interval.getMinimum())
-				&& (maximumIncluded || getMaximum() == interval.getMaximum());
+				&& (maximumIncluded || getMaximum() == interval
+						.getMaximum());
+	}
+
+	@Override
+	public boolean isIncluded(LeftOpenedInterval interval) {
+		boolean minimumIncluded = this.includes(interval.getMinimum());
+		boolean maximumIncluded = this.includes(interval.getMaximum());
+		return (minimumIncluded || getMinimum() == interval.getMinimum())
+				&& (maximumIncluded);
+	}
+
+	@Override
+	public boolean isIncluded(RightOpenedInterval interval) {
+		boolean minimumIncluded = this.includes(interval.getMinimum());
+		boolean maximumIncluded = this.includes(interval.getMaximum());
+		return (minimumIncluded)
+				&& (maximumIncluded || getMaximum() == interval
+						.getMaximum());
+	}
+
+	@Override
+	public boolean isIncluded(UnopenedInterval interval) {
+		boolean minimumIncluded = this.includes(interval.getMinimum());
+		boolean maximumIncluded = this.includes(interval.getMaximum());
+		return (minimumIncluded) && (maximumIncluded);
 	}
 
 	public boolean intersectsWith(Interval interval) {
