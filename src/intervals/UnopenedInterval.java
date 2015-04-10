03beecf25fa41e4c9642c370c7e934f1f1f29a3d
robinparadise <robinchogiles@gmail.com>
2015-04-10 22:49:30 +0100

    Fix includes(interval) method:: remove switch

--- src/intervals/UnopenedInterval.java
+++ src/intervals/UnopenedInterval.java
@@ -13,27 +13,8 @@ public class UnopenedInterval extends Interval {
 	public boolean includes(Interval interval) {
 		boolean minimumIncluded = this.includes(interval.getMinimum());
 		boolean maximumIncluded = this.includes(interval.getMaximum());
-		switch (interval.getOpening()) {
-		case BOTH_OPENED:
-			return (minimumIncluded || getMinimum() == interval.getMinimum())
-					&& (maximumIncluded || getMaximum() == interval
-							.getMaximum());
-		case LEFT_OPENED:
-			return (minimumIncluded || getMinimum() == interval.getMinimum())
-					&& (maximumIncluded || getMaximum() == interval
-							.getMaximum());
-		case RIGHT_OPENED:
-			return (minimumIncluded || getMinimum() == interval.getMinimum())
-					&& (maximumIncluded || getMaximum() == interval
-							.getMaximum());
-		case UNOPENED:
-			return (minimumIncluded || getMinimum() == interval.getMinimum())
-					&& (maximumIncluded || getMaximum() == interval
-							.getMaximum());
-		default:
-			assert false;
-			return false;
-		}
+		return (minimumIncluded || getMinimum() == interval.getMinimum())
+				&& (maximumIncluded || getMaximum() == interval.getMaximum());
 	}
 
 	public boolean intersectsWith(Interval interval) {
