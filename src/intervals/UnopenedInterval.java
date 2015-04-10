63137469ebe73d8fc6b9e405a827fe3141de00ca
robinparadise <robinchogiles@gmail.com>
2015-04-10 20:01:25 +0100

    Extract intersects with :: UnopenedInterval

--- src/intervals/UnopenedInterval.java
+++ src/intervals/UnopenedInterval.java
@@ -1,35 +1,52 @@
 package intervals;
 
 public class UnopenedInterval extends Interval {
-	
+
 	public UnopenedInterval(double minimum, double maximum, Opening opening) {
 		super(minimum, maximum, opening);
 	}
-	
+
 	public boolean includes(double value) {
 		return getMinimum() <= value && value <= getMaximum();
 	}
-	
+
 	public boolean includes(Interval interval) {
 		boolean minimumIncluded = this.includes(interval.getMinimum());
 		boolean maximumIncluded = this.includes(interval.getMaximum());
 		switch (interval.getOpening()) {
 		case BOTH_OPENED:
 			return (minimumIncluded || getMinimum() == interval.getMinimum())
-					&& (maximumIncluded || getMaximum() == interval.getMaximum());
+					&& (maximumIncluded || getMaximum() == interval
+							.getMaximum());
 		case LEFT_OPENED:
 			return (minimumIncluded || getMinimum() == interval.getMinimum())
-					&& (maximumIncluded || getMaximum() == interval.getMaximum());
+					&& (maximumIncluded || getMaximum() == interval
+							.getMaximum());
 		case RIGHT_OPENED:
 			return (minimumIncluded || getMinimum() == interval.getMinimum())
-					&& (maximumIncluded || getMaximum() == interval.getMaximum());
+					&& (maximumIncluded || getMaximum() == interval
+							.getMaximum());
 		case UNOPENED:
 			return (minimumIncluded || getMinimum() == interval.getMinimum())
-					&& (maximumIncluded || getMaximum() == interval.getMaximum());
+					&& (maximumIncluded || getMaximum() == interval
+							.getMaximum());
 		default:
 			assert false;
 			return false;
 		}
+	}
+
+	public boolean intersectsWith(Interval interval) {
+		if (getMinimum() == interval.getMaximum()) {
+			return interval.getOpening() == Opening.LEFT_OPENED
+					|| interval.getOpening() == Opening.UNOPENED;
+		}
+		if (getMaximum() == interval.getMinimum()) {
+			return interval.getOpening() == Opening.RIGHT_OPENED
+					|| interval.getOpening() == Opening.UNOPENED;
+		}
+		return this.includes(interval.getMinimum())
+				|| this.includes(interval.getMaximum());
 	}
 
 }
