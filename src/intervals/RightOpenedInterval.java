be68a314cdaedcc247a637a7e74d80d79efc53fa
robinparadise <robinchogiles@gmail.com>
2015-04-10 19:58:31 +0100

    Extract intersects with :: RightOpenedInterval

--- src/intervals/RightOpenedInterval.java
+++ src/intervals/RightOpenedInterval.java
@@ -1,28 +1,30 @@
 package intervals;
 
 public class RightOpenedInterval extends Interval {
-	
+
 	public RightOpenedInterval(double minimum, double maximum, Opening opening) {
 		super(minimum, maximum, opening);
 	}
-	
+
 	public boolean includes(double value) {
 		return getMinimum() <= value && value < getMaximum();
 	}
-	
+
 	public boolean includes(Interval interval) {
 		boolean minimumIncluded = this.includes(interval.getMinimum());
 		boolean maximumIncluded = this.includes(interval.getMaximum());
 		switch (interval.getOpening()) {
 		case BOTH_OPENED:
 			return (minimumIncluded || getMinimum() == interval.getMinimum())
-					&& (maximumIncluded || getMaximum() == interval.getMaximum());
+					&& (maximumIncluded || getMaximum() == interval
+							.getMaximum());
 		case LEFT_OPENED:
 			return (minimumIncluded || getMinimum() == interval.getMinimum())
 					&& (maximumIncluded);
 		case RIGHT_OPENED:
 			return (minimumIncluded || getMinimum() == interval.getMinimum())
-					&& (maximumIncluded || getMaximum() == interval.getMaximum());
+					&& (maximumIncluded || getMaximum() == interval
+							.getMaximum());
 		case UNOPENED:
 			return (minimumIncluded || getMinimum() == interval.getMinimum())
 					&& (maximumIncluded);
@@ -30,6 +32,18 @@ public class RightOpenedInterval extends Interval {
 			assert false;
 			return false;
 		}
+	}
+
+	public boolean intersectsWith(Interval interval) {
+		if (getMinimum() == interval.getMaximum()) {
+			return interval.getOpening() == Opening.LEFT_OPENED
+					|| interval.getOpening() == Opening.UNOPENED;
+		}
+		if (getMaximum() == interval.getMinimum()) {
+			return false;
+		}
+		return this.includes(interval.getMinimum())
+				|| this.includes(interval.getMaximum());
 	}
 
 }
