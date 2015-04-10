3675a96100360100bb7adc98f47319af2a0c991c
robinparadise <robinchogiles@gmail.com>
2015-04-10 19:05:14 +0100

    LeftOpenendInterval::Extract includes interval

--- src/intervals/LeftOpenedInterval.java
+++ src/intervals/LeftOpenedInterval.java
@@ -9,5 +9,27 @@ public class LeftOpenedInterval extends Interval {
 	public boolean includes(double value) {
 		return getMinimum() < value && value <= getMaximum();
 	}
+	
+	public boolean includes(Interval interval) {
+		boolean minimumIncluded = this.includes(interval.getMinimum());
+		boolean maximumIncluded = this.includes(interval.getMaximum());
+		switch (interval.getOpening()) {
+		case BOTH_OPENED:
+			return (minimumIncluded || getMinimum() == interval.getMinimum())
+					&& (maximumIncluded || getMaximum() == interval.getMaximum());
+		case LEFT_OPENED:
+			return (minimumIncluded || getMinimum() == interval.getMinimum())
+					&& (maximumIncluded || getMaximum() == interval.getMaximum());
+		case RIGHT_OPENED:
+			return (minimumIncluded)
+					&& (maximumIncluded || getMaximum() == interval.getMaximum());
+		case UNOPENED:
+			return (minimumIncluded)
+					&& (maximumIncluded || getMaximum() == interval.getMaximum());
+		default:
+			assert false;
+			return false;
+		}
+	}
 
 }
